b39192fe66ba31260fb85eec0d0d93cda40bbaa7
Saloni Agrahari <121040307+SaloniAgrahari7@users.noreply.github.com>
2023-01-31 21:07:40 +0545

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,36 @@
+
+<nav class="navbar navbar-expand-lg navbar-light bg-white  px-lg-3 py-lg-2 shadow-sm sticky-top">
+    <div class="container-fluid">
+      <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="home.php">SKPP RETRO AND LODGE</a>
+      <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active me-2" aria-current="page" href="home.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="room.php">Rooms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-2" href="contact.php">Contact Us</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link me-2" href="about.php">About Us</a>
+          </li>
+
+
+          
+        </li>
+
+      </ul>
+      <div class="d-flex">
+        <!-- <button type="button" class="btn btn-light"  data-bs-toggle="modal" data-bs-target="#loginmodal"> <a href="login.php">Customer Login</a></button> -->
+        <button type="button" class="btn btn-light"  data-bs-toggle="modal" data-bs-target="#loginmodal"> <a href="Admin/index.php"> Login</a></button>
+        <!-- <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#loginmodal" ><a href="register.php"> Register</a></button> -->
+      </div>
+    </div>
+  </div>
+</nav>
---
+++ inc/link.php
@@ -0,0 +1,5 @@
+   
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+     <link rel="stylesheet" type="text/css" href="css/style.css">
